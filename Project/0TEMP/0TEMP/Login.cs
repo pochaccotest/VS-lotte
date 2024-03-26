225856a00018a0cea1dbcf5aa99d1d44b9fa363e
DuyenLy <ldcc.ngocphu@lotte.vn>
2024-03-26 18:08:55 +0700

    add new file
    add new file

---
+++ Project/0TEMP/0TEMP/Login.cs
@@ -0,0 +1,84 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+[TestFixture]
+public class LoginPageTest : IDisposable
+{
+    private IWebDriver? driver;
+
+    public object? ScreenshotImageFormat { get; private set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        var chromeDriverDirectory = @"D:\Duyen\VisualStudio\chromedriver.exe";
+        var chromeOptions = new ChromeOptions();
+        chromeOptions.BinaryLocation = @"D:\Duyen\VisualStudio\GoogleChromePortable64\App\Chrome-bin\Chrome.exe";
+
+        driver = new ChromeDriver(chromeDriverDirectory, chromeOptions);
+    }
+
+    [Test]
+    public void VerifyLogin()
+    {
+        driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        Thread.Sleep(1000);
+        driver.FindElement(By.XPath("//input[@placeholder='Username']")).SendKeys(" ");
+        driver.FindElement(By.XPath("//input[@placeholder='Password']")).SendKeys(" ");
+        driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+        Thread.Sleep(2000);
+        //driver.FindElement(By.LinkText("My Info")).Click();
+        try
+        {
+            // find an element invalid
+            IWebElement MsgInva = driver.FindElement(By.ClassName("oxd-alert-content-text"));
+            // store text of an element in a string
+            String vMsgInva = MsgInva.Text;
+            if (vMsgInva.Contains("Invalid credentials"))
+            {
+                Console.WriteLine($"Testcase Pass");
+                Thread.Sleep(2000);
+                // Take a screenshot and save it to a file
+                ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile($"..\\..\\..\\..\\TestResult\\Invalid.png");
+            }
+        }
+        catch
+        {           
+                // find an element require username
+                IWebElement MsgRqU = driver.FindElement(By.XPath("//div[@class='orangehrm-login-form']//div[2]//div[1]//span[1]"));
+                // store text of an element in a string
+                String vMsgRqU = MsgRqU.Text;
+                if (vMsgRqU.Contains("Required"))
+                {
+                    Thread.Sleep(2000);
+                    // Take a screenshot and save it to a file
+                    ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile($"..\\..\\..\\..\\TestResult\\RequiredU.png");
+                }
+            
+                // find an element require password
+                IWebElement MsgRqP = driver.FindElement(By.XPath("//div[@class='orangehrm-login-slot-wrapper']//div[1]//div[1]//span[1]"));
+                // store text of an element in a string
+                String vMsgRqP = MsgRqP.Text;
+                if (vMsgRqP.Contains("Required"))
+                {
+                    Thread.Sleep(2000);
+                    // Take a screenshot and save it to a file
+                    ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile($"..\\..\\..\\..\\TestResult\\RequiredP.png");
+                }           
+        }
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Quit();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Project/0TEMP/0TEMP/Program.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+public class LoginTest
+{
+    private IWebDriver driver;
+    private string filePath = @"path\to\your\file.csv"; // Update with the path to your CSV file
+
+    public LoginTest()
+    {
+        driver = new ChromeDriver();
+    }
+
+    public void ReadDataAndLogin()
+    {
+        using (var reader = new StreamReader(filePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                string username = values[0];
+                string password = values[1];
+
+                // Now use these credentials to login
+                Login(username, password);
+            }
+        }
+    }
+
+    private void Login(string username, string password)
+    {
+        // Navigate to the login page
+        driver.Navigate().GoToUrl("http://your-login-page.com");
+
+        // Find the username field and enter the username
+        driver.FindElement(By.Id("username")).SendKeys(username);
+
+        // Find the password field and enter the password
+        driver.FindElement(By.Id("password")).SendKeys(password);
+
+        // Find the login button and click it
+        driver.FindElement(By.Id("loginButton")).Click();
+    }
+}
